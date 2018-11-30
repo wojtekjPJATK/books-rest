5310202e637eb4ee7ceb4450c3117af0672cb8c4
wojtekjPJATK <s15983@pjwstk.edu.pl>
2018-11-30 17:38:03 +0100

    flasek golasek

--- apis/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask_restplus import Api
-
-from .book import api as books
-from .auth import api as auth
-
-api = Api(version='0.1', title='Book API', description='An API for books')
-
-api.add_namespace(books)
-api.add_namespace(auth)
--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from flask import Flask
-
-
-from apis import api
-
-
 app = Flask(__name__)
-api.init_app(app)
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/")
+def hello():
+    return "Hello World!"
