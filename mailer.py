11173004f4d32d9f4f7001567c84db5c261e8126
wojtekjPJATK <s15983@pjwstk.edu.pl>
2018-12-04 19:24:33 +0100

    added task queue

---
+++ mailer.py
@@ -0,0 +1,20 @@
+import webapp2
+import requests
+from requests_toolbelt.adapters import appengine
+
+
+class SendEmail(webapp2.RequestHandler):
+    def post(self):
+        return requests.post(
+            "https://api.mailgun.net/v3/sandboxf69711266f1e4f3fa5782627f0edccf8.mailgun.org/messages",
+            auth=("api", "d5dd6bdd709ceec36eb7180f6ce7377d-52cbfb43-79c6b6b8"),
+            data={"from": "Mailgun Sandbox <postmaster@sandboxf69711266f1e4f3fa5782627f0edccf8.mailgun.org>",
+                  "to": "Wojciech Jarmakowski <s15983@pjwstk.edu.pl>",
+                  "subject": "Hello",
+                  "text": "Congratulations you just sent an email with Mailgun using task queue!  You are truly awesome!"})
+
+
+appengine.monkeypatch()
+app = webapp2.WSGIApplication([
+    ('/send', SendEmail)
+], debug=True)
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from google.appengine.api import taskqueue
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+    
+@app.route("/mail")
+def send():
+	task = taskqueue.add(
+            url='/send',
+            target='mailer')
+
+        self.response.write(
+            'Task {} enqueued, ETA {}.'.format(task.name, task.eta))
