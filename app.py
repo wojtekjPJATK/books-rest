16179461801503bd658b9703441685b3d134892b
Wojtek J <wojtekj1989@gmail.com>
2018-12-16 21:24:42 +0100

    Pubsub (#4)
    * tetsing datastore connection
    
    * lol
    
    * ndb con
    
    * task queue fix
    
    * libs added
    
    * pubsub test
    
    * added libs
    
    * added lib files
    
    * added message publisher
    
    * added pubsub subscriber save msg to datastore
    
    * cfg update

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from info import Info
 from task import TaskQueue
 from login import Login
 from user import User
+from publisher import PubSubPublisher
+from subscriber import PubSubSubscriber
 import cloudstorage
 from google.appengine.api import app_identity
 
@@ -18,3 +20,5 @@ api.add_resource(Info, '/')
 api.add_resource(TaskQueue, '/mail')
 api.add_resource(Login, '/login')
 api.add_resource(User, '/user')
+api.add_resource(PubSubPublisher, '/pubsub')
+api.add_resource(PubSubSubscriber, '/_ah/push-handlers/dummypush')
