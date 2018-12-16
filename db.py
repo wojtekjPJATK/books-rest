569ddabc22f2ec91344c10237142bd5cc245c12e
Wojtek <wojtekj1989@gmail.com>
2018-12-16 21:34:16 +0100

    refactor

--- db.py
+++ db.py
@@ -3,26 +3,28 @@ import json
 
 
 class UserTest(ndb.Model):
-  username = ndb.StringProperty()
-  password = ndb.StringProperty()
-    
+    username = ndb.StringProperty()
+    password = ndb.StringProperty()
+
+
 class Message(ndb.Model):
-  text = ndb.StringProperty()
-  msg = ndb.StringProperty()
-	
+    text = ndb.StringProperty()
+    msg = ndb.StringProperty()
+
+
 def postMessage(data, content):
-  message = Message()
-  message.text = str(data)
-  message.msg = str(content)
-  key = message.put()
-  id = key.id()
-  return id
-    
+    message = Message()
+    message.text = str(data)
+    message.msg = str(content)
+    key = message.put()
+    id = key.id()
+    return id
+
+
 def postUser():
-  user = UserTest()
-  user.username = "Adam22"
-  user.password = 'admin'
-  key = user.put()
-  id = key.id()
-  return {'username': user.username, 'id': id}
-    
+    user = UserTest()
+    user.username = "Adam22"
+    user.password = 'admin'
+    key = user.put()
+    id = key.id()
+    return {'username': user.username, 'id': id}
--- info.py
+++ info.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
 
+
 class Info(Resource):
     def get(self):
         return {'message': 'mainpage hello world'}
