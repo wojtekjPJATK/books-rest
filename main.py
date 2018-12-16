d9f25f02ba190a73a62b3696cd10709e108c26ed
Sosuno <ewa.jasinska.02@gmail.com>
2018-12-16 22:08:19 +0100

    'backend'

---
+++ main.py
@@ -0,0 +1,276 @@
+import model_datastore
+from flask import Flask, request, jsonify, session, abort
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+model = model_datastore
+
+@app.route("/signin", methods = ['POST'])
+def signin():
+        content = request.get_json()
+        if content is None:
+                return jsonify(msg = "No JSON for me huh?")
+        if model.isCorrectUser(content['login'].strip(), content['password'].strip()):
+                model.destroyAllUserSessions(content['login'].strip())
+                my_session = model.createSession(content['login'].strip())
+                return jsonify(id = my_session['sessionID'])
+        else:
+                return jsonify(msg = "Wrong username or password")
+
+@app.route("/join", methods = [ 'POST' ])
+def join():
+        content = request.get_json()
+        if content is None:
+                return jsonify(msg = "No JSON for me huh?")
+        elif content.get('login') is None:
+                return jsonify(msg = "Forgot my login")
+        username = content['login'].strip()
+        if not username:
+                return jsonify(msg = "Forgot my login")
+        if not model.isUserInDB(username):
+                data = {}
+                data['username'] = content['login'].strip()
+                data['password'] = content['password'].strip()
+                data['security_lvl'] = 'normal'
+                data['favBooks'] = None
+                model.create(data)
+                my_session = model.createSession(content['login'])
+                return jsonify(id = my_session['sessionID'])
+        else:
+                return jsonify(msg = "User alredy exists")
+
+@app.route("/oauth", methods = [ "POST" ])
+def oauth():
+        content = request.get_json()
+        if content is None:
+                return jsonify(msg = "Forgot my email")
+        elif content.get('email') is None:
+                return jsonify(msg = "Forgot my email")
+        username = content['email'].strip()
+        if not username:
+                return jsonify(msg = "Forgot my email")
+        if model.isUserInDB(username):
+                model.destroyAllUserSessions(username)
+                my_session = model.createSession(username)
+        else:
+                data = {}
+                data['username'] = username
+                data['password'] = None
+                data['security_lvl'] = 'normal'
+                data['favBooks'] = None
+                data['origin'] = 'google'
+                model.create(data)
+                my_session = model.createSession(username)
+        
+        return jsonify(id = my_session['sessionID'])
+
+
+@app.route("/book", methods = [ 'GET', 'POST'])
+def books():
+        uuid = None
+        if 'Authorization' not in request.headers:
+                return abort(401)
+        uuid = request.headers.get('Authorization')
+        if uuid is None:
+                return abort(401)
+        if not model.checkIfSessionActive(uuid):
+                return abort(401)
+        username = model.getUsernameFromSession(uuid)
+        user = model.getUser(username) 
+        if user is None:
+                return jsonify(msg = "Something went horribly wrong")                   
+        if request.method == 'GET':
+                books = model.BookList()
+                if user['favBooks'] is not None:
+                        favBooks = list((user['favBooks']))
+                else: 
+                        favBooks = []
+                return jsonify(books = books, favBooks = favBooks)
+        else:
+                content  = request.get_json()
+                if not model.checkIfBookExists(content['author'], content['title'].strip()):
+                        #img = request.files.get('image')          
+                        #image_url = model.upload_image_file(img)
+                        data = {}
+                        data['title'] = content['title'].strip()
+                        data['author'] = list(content['author'])
+                        data['description'] = content.get('description')
+                        data['imageUrl'] = None 
+                        data['addedBy'] = user['id']
+                        book = model.createBook(data)
+                        return jsonify(book = book)
+                else:
+                        return jsonify(msg = "Book already exists")
+
+@app.route("/cover/<id>", methods = ['POST'])
+def getCover(id):
+        uuid = None
+        if 'Authorization' not in request.headers:
+                return abort(401)
+        uuid = request.headers.get('Authorization')
+        if uuid is None:
+                return abort(401)
+        if not model.checkIfSessionActive(uuid):
+                return abort(401)
+        if id is None:
+                return jsonify(msg = 'Forgot id')        
+        book = model.BookRead(id)
+        if book is None:
+                return jsonify(msg = "No book with this id")
+        img = request.files.get('image')
+        image_url = model.upload_image_file(img)
+        book['imageUrl'] = image_url
+        book = model.BookUpdate(book, id)
+        return jsonify(book = book)
+
+@app.route("/book/<id>", methods = ['GET', 'PATCH', 'DELETE'])
+def book(id):
+        uuid = None
+        if 'Authorization' not in request.headers:
+                return abort(401)
+        uuid = request.headers.get('Authorization')
+        if uuid is None:
+                return abort(401)
+        if not model.checkIfSessionActive(uuid):
+                return abort(401)
+        if id is None:
+                return jsonify(msg = 'Forgot id')
+        if request.method == 'GET':
+                book = model.BookRead(id)
+                if book is not None:
+                        return jsonify(book = book)
+                else:
+                        return jsonify(msg = "No book with this id")
+
+        elif request.method == 'PATCH':
+                book = model.BookRead(id)
+                if book is not None:
+                        content = request.get_json()
+                        if 'title' in content:
+                                book['title'] = content['title'].strip()
+                        if 'author' in content:        
+                                book['author'] = list(content['author'])
+                        if 'description' in content:
+                                book['description'] = content.get('description')
+                        book = model.BookUpdate(book, id)
+                        return jsonify(book = book)
+                else:
+                        return jsonify(msg = "No book with this id")
+        else:
+                model.BookDelete(id)
+                return jsonify(msg = "Deleted")
+
+@app.route("/author", methods = ['GET', 'POST'])
+def authors():
+        uuid = None
+        if 'Authorization' not in request.headers:
+                return abort(401)
+        uuid = request.headers.get('Authorization')
+        if uuid is None:
+                return abort(401) 
+        if not model.checkIfSessionActive(uuid):
+                return abort(401)
+        if request.method == 'GET':
+                authors = model.AuthorList()
+                return jsonify(authors = authors)
+        else:
+                content = request.get_json()
+                if not model.isAuthorInDB(content['firstName'].strip(), content['lastName'].strip()):
+                        data = {}
+                        data['firstName'] = content['firstName'].strip()
+                        data['lastName'] = content['lastName'].strip()
+                        author = model.createAuthor(data)
+                        return jsonify(author = author)
+                else:
+                        return jsonify(msg = "Author already in database")
+
+@app.route("/author/<id>", methods = [ 'PATCH', 'DELETE'])
+def author(id):
+        uuid = None
+        if 'Authorization' not in request.headers:
+                return abort(401)
+        uuid = request.headers.get('Authorization')
+        if uuid is None:
+                return abort(401) 
+        if not model.checkIfSessionActive(uuid):
+                return abort(401)     
+        if id is None:
+                return jsonify(msg = 'Forgot id')
+        if request.method == 'PATCH':
+                author = model.AuthorRead(id)
+                if author is not None:
+                        content = request.get_json()
+                        if 'firstName' in content:
+                                author['firstName'] = content['firstName'].strip()
+                        if 'lastName' in content:
+                                author['lastName'] = content['lastName'].strip()
+                        author = model.AuthorUpdate(author,id)
+                        return jsonify(author = author)
+                else:
+                        return jsonify(msg = "No author with this id")
+        else:
+                model.AuthorDelete(id) 
+                return jsonify(msg = "Deleted")
+
+@app.route("/favorites/<id>", methods = ['POST'])
+def favBooks(id):
+        uuid = None
+        if 'Authorization' not in request.headers:
+                return abort(401)
+        uuid = request.headers.get('Authorization')
+        if uuid is None:
+                return abort(401)
+        if not model.checkIfSessionActive(uuid):
+                return abort(401)  
+        if id is None:
+                return jsonify(msg = 'Forgot id')
+        username = model.getUsernameFromSession(uuid)
+        user = model.getUser(username) 
+        favBooks = user['favBooks']
+        if favBooks is None:
+                favBooks = []
+                favBooks.append(id)
+        elif id in favBooks:
+                favBooks.remove(id)
+                if not favBooks:
+                        favBooks = None
+        else:
+                favBooks.append(id)
+        
+        user['favBooks'] = favBooks        
+        user = model.UserUpdate(user, user['id'])
+        return jsonify(favBooks = user['favBooks'])
+
+@app.route("/session/<id>", methods = [ 'GET' , 'DELETE' ])
+def sessions(id):
+        uuid = None
+        if 'Authorization' not in request.headers:
+                return abort(401)
+        uuid = request.headers.get('Authorization')
+        if uuid is None:
+                return abort(401)
+        if not model.checkIfSessionActive(uuid):
+                return abort(401)
+        if request.method == 'GET':
+                if not model.checkIfSessionActive(uuid):
+                        return jsonify(session = False)
+                else:
+                        return jsonify(session = True)
+                #username = model.getUsernameFromSession(uuid)
+                #user = model.getUser(username) 
+                #return jsonify(user = user)
+        else:
+                if not model.checkIfSessionActive(uuid):
+                        return abort(401)
+                model.destroySession(uuid)
+                return jsonify(msg = "Deleted")
+
+
+  
+
+@app.errorhandler(401)
+def session_not_found(e):
+    return "<p> Error 401. Session not found </p>"
+
---
+++ model_datastore.py
@@ -0,0 +1,328 @@
+from st import storage
+import uuid 
+from flask import current_app
+from google.cloud import datastore
+
+
+builtin_list = list
+
+
+def init_app(app):
+    pass
+
+def get_client():
+    return datastore.Client('solwit-pjatk-arc-2018-gr4')
+
+def from_datastore(entity):
+    if not entity:
+        return None
+    if isinstance(entity, builtin_list):
+        entity = entity.pop()
+
+    entity['id'] = entity.key.id
+    return entity
+
+#---------------------------------> BOOKS
+
+def BookList():
+    ds = get_client()
+    query = ds.query(kind='Book', order=['title'])
+    results = list(query.fetch())
+    entities = []
+    for result in results:
+        entities.append(from_datastore(result))
+        
+    return entities
+
+def BookRead(id):
+    ds = get_client()
+    key = ds.key('Book', int(id))
+    results = ds.get(key)
+    if results is None:
+        return None
+    return from_datastore(results)
+
+def BookUpdate(data, id=None):
+    ds = get_client()
+    if id:
+        key = ds.key('Book', int(id))
+    else:
+        key = ds.key('Book')
+
+    entity = datastore.Entity(
+        key=key,
+        exclude_from_indexes=['description'])
+
+    if 'id' in data:
+        del data['id']
+
+    entity.update(data)
+    ds.put(entity)
+    return from_datastore(entity)
+
+createBook = BookUpdate
+
+def BookDelete(id):
+    ds = get_client()
+    key = ds.key('Book', int(id))
+    ds.delete(key)
+
+def upload_image_file(file):
+    if not file:
+       return None
+
+    public_url = storage.upload_file(
+        file.read(),
+        file.filename,
+        file.content_type
+    )
+
+    current_app.logger.info(
+        "Uploaded file %s as %s.", file.filename, public_url)
+
+    return public_url
+
+# do usunięcia prawdopodobnie
+def getBookByTitle(title):
+    ds = get_client()
+    query = ds.query(kind = 'Book')
+    books = query.fetch()
+    results = []
+    for book in books:
+        if title.lower() in book['title'].lower():
+            results.append(from_datastore(book))
+    
+    return results
+
+# do usunięcia prawdopodobnie
+def getBookByAuthor(author):
+    ds = get_client()
+    query = ds.query(kind = 'Book')
+    books = query.fetch()
+    results = []
+    for book in books:
+        if author.lower() in book['author'].lower():
+            results.append(from_datastore(book))
+    
+    return results
+
+# search barbaric style 
+def findBookByAnything(string):
+    ds = get_client()
+    query = ds.query(kind = 'Book')
+    books = query.fetch()
+    results = []
+    string = string.lower()
+    for book in books:
+        if string in book['title'].lower():
+            results.append(from_datastore(book))
+        elif string in book['description'].lower():
+            results.append(from_datastore(book))
+        else:
+            for author in book['author']:
+                if string in author.lower():
+                    results.append(from_datastore(book))
+                    break
+    return results              
+        
+
+def checkIfBookExists(author, title):
+    ds = get_client()
+    query = ds.query(kind = 'Book')
+    #query.add_filter('author', '=', author)
+    query.add_filter('title', '=', title)
+    results = list(query.fetch())
+
+    if results is None:
+        return False
+    elif not results:
+        return False
+    else:
+        return True
+
+#---------------------------------> AUTHORS
+
+def AuthorRead(id):
+    ds = get_client()
+    key = ds.key('Author', int(id))
+    results = ds.get(key)
+    if results is None:
+        return None
+    return from_datastore(results)
+
+def AuthorUpdate(data, id=None):
+    ds = get_client()
+    if id:
+        key = ds.key('Author', int(id))
+    else:
+        key = ds.key('Author')
+
+    entity = datastore.Entity(
+        key=key
+       )
+    if 'id' in data:
+        del data['id']
+    entity.update(data)
+    ds.put(entity)
+    return from_datastore(entity)
+
+createAuthor = AuthorUpdate
+
+def AuthorList():
+    ds = get_client()
+    query = ds.query(kind='Author', order=['lastName'])
+    results = query.fetch()
+    alist = []
+    for result in results:
+        alist.append(from_datastore(result))
+    return alist
+
+def AuthorDelete(id):
+    ds = get_client()
+    key = ds.key('Author', int(id))
+    ds.delete(key)
+
+def isAuthorInDB(name, surname):
+    ds = get_client()
+    query = ds.query(kind='Author')
+    query.add_filter('firstName', '=', name)
+    query.add_filter('lastName', '=', surname)
+    result = list(query.fetch())
+    if result:
+        return True
+    else:
+        return False
+
+def getAuthor(name, surname):
+    ds = get_client()
+    query = ds.query(kind = 'Author')
+    query.add_filter('firstName', '=', name)
+    query.add_filter('lastName', '=', surname)
+    result = list(query.fetch())
+    return from_datastore(result)
+
+#---------------------------------> USERS
+    
+def getUser(username):
+    ds = get_client()
+    query = ds.query(kind = 'User')
+    query.add_filter('username', '=', username)
+    result = list(query.fetch())
+    
+    if result is None:
+        return None
+    elif not result:
+        return None
+    r = result.pop()
+    return from_datastore(r) 
+
+def UserRead(id):
+    ds = get_client()
+    key = ds.key('User', int(id))
+    results = ds.get(key)
+    return from_datastore(results)
+
+def UserUpdate(data, id=None):
+    ds = get_client()
+    if id:
+        key = ds.key('User', int(id))
+    else:
+        key = ds.key('User')
+    entity = datastore.Entity(
+        key=key,
+        )
+    if 'id' in data:
+        del data['id']
+    entity.update(data)
+    ds.put(entity)
+    return from_datastore(entity)
+
+create = UserUpdate
+
+def UserDelete(id):
+    ds = get_client()
+    key = ds.key('User', int(id))
+    ds.delete(key)
+
+def isUserInDB(username):
+    ds = get_client()
+    query = ds.query(kind='User')
+    query.add_filter('username', '=', username)
+    result = list(query.fetch())
+    if result:
+        return True
+    else:
+        return False
+
+def isCorrectUser(username, password):
+    ds = get_client()
+    query = ds.query(kind = 'User')
+    query.add_filter('username', '=', username)
+    query.add_filter('password', '=', password)
+    result = list(query.fetch())
+    if result:
+        return True
+    else:
+        return False
+
+#---------------------------------> SESSION
+
+def createSession(username):
+    ds = get_client()
+
+    key = ds.key('Session')
+    entity = datastore.Entity(key=key)
+    entity.update({
+        'sessionID': str(uuid.uuid1()),
+        'user': username,
+        'status': 'active'
+    })
+    ds.put(entity)
+    return entity
+
+def destroySession(uuid):
+    ds = get_client()
+    query = ds.query(kind = 'Session')
+    query.add_filter('sessionID', '=', uuid)
+    results = list(query.fetch())
+    for result in results:
+        r = from_datastore(result)
+        key = ds.key('Session', int(r['id']))
+        ds.delete(key)
+
+def destroyAllUserSessions(username):
+    ds = get_client()
+    query = ds.query(kind = 'Session')
+    query.add_filter('user', '=', username)
+    results = list(query.fetch())
+
+    for result in results:
+        r = from_datastore(result)
+        key = ds.key('Session', int(r['id']))      
+        ds.delete(key)
+
+def checkIfSessionActive(uuid):
+    ds = get_client()
+    query = ds.query(kind = 'Session')
+    query.add_filter('sessionID', '=', uuid)
+    query.add_filter('status', '=', 'active')
+    result = list(query.fetch())
+    if result is None:
+        return False
+    elif not result:
+        return False
+    else:
+        return True
+
+def getUsernameFromSession(uuid):
+    ds = get_client() 
+    query = ds.query(kind = 'Session')
+    query.add_filter('sessionID', '=', uuid)
+    result = list(query.fetch())
+    if result is None:
+        return None
+    elif not result:
+        return None
+    else:
+        r = result.pop()
+        return r['user']
